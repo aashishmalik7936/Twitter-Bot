dc59f6633c19b05ca1b799913a983a88a9bb3623
Aashish Malik <45069658+aashishmalik7936@users.noreply.github.com>
2019-11-23 16:13:22 +0530

    Add files via upload

---
+++ reply_to_mentions.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+consumer_key="yyjbA1N5m2sIxBCEGtsy5wOW9";
+consumer_secret="1ZNU6d9P0HTGVNRECHvvSqrwFcd7k1iVspJ4irmie7WVQNDVfd";
+access_token_key="1108065502969167872-whFkt1iPDVk067kP27b7qNTGxhNnv2";
+access_token_secret="YC0qXzT17DD7EnUOSxaMydtFTZmH3P97Gc7dbV6ICRFwm";
+
+
+# In[ ]:
+
+
+# Reply to mentions bot
+import tweepy
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.mentions_timeline,
+        since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info(f"Answering to {tweet.user.name}")
+
+            if not tweet.user.following:
+                tweet.user.follow()
+
+            api.update_status(
+                status="Please reach us via DM",
+                in_reply_to_status_id=tweet.id,
+            )
+    return new_since_id
+
+def main():
+    api = create_api()
+    since_id = 1
+    while True:
+        since_id = check_mentions(api, ["help", "support"], since_id)
+        logger.info("Waiting...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
+
